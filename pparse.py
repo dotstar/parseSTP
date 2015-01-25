29b7e875d54eee7615b2f06c76a60926b654f158
dotstar <cddickerson@gmail.com>
2015-01-25 12:57:12 -0600

    modified:   pparse.py

--- pparse.py
+++ pparse.py
@@ -25,10 +25,216 @@ import gzip
 from glob import iglob
 import io
 import re
+import sys
 import numpy as np
 import multiprocessing as mp
+import logging
+from threading import Timer
 import time
 
+
+
+
+def gettable(stpfile, start, stop, never):
+    # Find the top of the table
+    # returns the lines from the STP file between start and stop
+    # returns "None" on EOF
+    print 'gettable called: (', stpfile, ",", "start", ",", "stop", ",", never, ')'
+    debug = 0
+    if never is None:
+        never = 'xyzzy123____5551212'
+    startmatch = re.compile(start)
+    stopmatch = re.compile(stop)
+    diematch = re.compile(never)
+
+    (patternfound, rc) = (skipto(stpfile, startmatch, diematch))
+    if rc:
+        returnbuffer = io.StringIO()
+        rc = False
+        if debug > 14:
+            print 'patternfound:', patternfound
+        if patternfound is not None:
+            # Process to the bottom of the table ...
+            # for stpbuffer in stpfile:
+            while True:
+                stpbuffer = stpfile.readline()
+                if stpbuffer == '':
+                    break
+
+                if stopmatch.match(stpbuffer):
+                    # print 'found the terminating regExp'
+                    rc = True
+                    break
+                elif diematch.match(stpbuffer):
+                    returnbuffer = stpbuffer
+                    rc = False
+                    break
+                elif stpbuffer is None:
+                    returnbuffer = None
+                else:
+                    returnbuffer.write(unicode(stpbuffer))
+        else:
+            returnbuffer = None
+            patternfound = ''
+            rc = False
+    else:
+        patternfound = None
+        returnbuffer = None
+        rc = False
+    return patternfound, returnbuffer, rc
+
+def skipto(stpfile, firstregx, dieregx):
+    print "skipto(", ",", stpfile, ",", 'firstRE', ",", dieregx, ")"
+    # Look forward into the file
+    # searching for RE or dieRE
+    # return rc = True if matching RE
+    # return rc = False if matching dieRE
+    found = False
+    debug = 20
+    i = 0
+    while not found:
+        fileptr = stpfile.tell()  # Where are we before the read?
+        stpline = stpfile.readline()
+        if stpline == '':
+            return '', False  # EOF?
+        i += 1
+        if firstregx.match(stpline):
+            if debug > 19:
+                print 'matched firstRE', stpline  # print 'skipped',i,'lines'
+            return stpline, True
+        elif dieregx.match(stpline):
+            if debug > 19:
+                print 'matched dieRE', stpline
+            stpfile.seek(fileptr)  # roll the file back
+            print 'skipped', i, 'lines'
+            return stpline, False
+
+def tsdecode(ts):
+    # Input - a time stamp from STP formated like: <TIMESTAMP: 20141107, 080003>
+    # Returns - ctime long integer
+
+    sts = ts.split()
+    year = int((sts[1])[0:4])
+    month = int((sts[1])[4:6])
+    day = int((sts[1][6:8]))
+    hour = int((sts[2][0:2])) - 1
+    minute = int((sts[2][2:4]))
+    second = int((sts[2][4:6]))
+    t = (year, month, day, hour, minute, second, 0, 0, -1)
+    # print ts, year, month, day, hour, minute, second
+    retval = int(time.mktime(t))
+    return retval
+
+def setval(trackingvar, table, i, rateflag):
+    # Tag column i of table as a rate field
+    # This is probably very un-pythonesque.
+    # if the table hasn't been seen before, create a row
+    global maxrow
+    try:
+        trackingvar[table][i] = rateflag
+        rc = True
+    except KeyError:
+        row = [None] * maxrow
+        trackingvar[table] = row
+        trackingvar[table][i] = rateflag
+        rc = True
+    return rc
+
+def getval(trackingvar, table, i):
+    # Given a table name and column
+    # Returns True if this is
+    # rate column
+    if not trackingvar[table][i]:
+        rc = None
+    else:
+        rc = trackingvar[table][i]
+    return rc
+
+def printdb(trackingvar):
+    # Convenience routine for debugging
+    for key in trackingvar:
+        print key,
+        i = 0
+        while trackingvar[key][i] is not None:
+            print trackingvar[key][i],
+            i += 1
+        print
+
+def processheaders(fp):
+    # Is there a clean way to do this without reading the whole file ?
+    # Does it matter if the net effect is that the file will be in buffer cache for the 2nd pass ?
+    print "Processing Headers and Metrics"
+    debug = 10
+    while True:
+        (firstline, tabletext, rc) = gettable(fp, '^<METRIC:', '^<END>', '^<TIMESTAMP: ')
+        # if tabletext is None or rc == False:
+        if tabletext is None:
+            print 'out of metrics?'
+            break  # We either ran out of Metrics in the file or didn't see an END.
+        else:
+            tablename = firstline[9:-3]
+            # For unknown reasons, 4 variables are named RDF-* in the metric section and Rdf-* in the Data section
+            # Patch around this for now.
+            if tablename[0:4] == 'RDF-':
+                tablename = re.sub("RDF-", "Rdf-", tablename)
+            if debug > 9:
+                print "Metric Name: " + tablename
+            mybuffer = tabletext.getvalue().split("\n")
+            columns = []
+            colnum = 0
+            for line in mybuffer:
+                # For each column there is a CSV with a name and some attributes.
+                # Similar to:
+                # symid,long,Key,ArchiveLast
+                # ios per sec,longlong,ConvertToRate,ArchiveStats,sortDescending
+                # reads per sec,longlong,ConvertToRate,ArchiveStats
+
+                if line != '':
+                    linevalues = line.split(',')
+                    columnname = linevalues[0]
+                    columntype = linevalues[1]
+                    setval(typetable, tablename, colnum, columntype)
+                    columnaction = linevalues[2]
+                    if columnaction == "Derived":
+                        setval(ratetable, tablename, colnum, False)
+                    elif columnaction == "ConvertToRate":
+                        setval(ratetable, tablename, colnum, True)
+                    else:
+                        setval(ratetable, tablename, colnum, False)
+                    setval(headertable, tablename, colnum, columnname)
+                    columns.append(columnname)
+                colnum += 1
+            headers[tablename] = columns
+    # Build header string in global variable, one for each table.
+
+    return headers, True
+
+def openoutputfile(filename, directoryname, headers):
+    # Convenience routine
+    # Check to see if the file exists.  If not, create it and place headers
+    # else open for append
+    fullyqualifiedname = directoryname + '/' + filename
+
+    if not os.path.isfile(fullyqualifiedname):
+        # Here when the file doesn't already exist
+        f1 = open(fullyqualifiedname, 'a')
+        f1.write(headers)
+    else:
+        f1 = open(fullyqualifiedname, 'a')
+    return f1
+
+def openinputfile(name, mode):
+    # print 'file is:', name
+    # print 'directory is:',os.getcwd()
+    m = re.search('.*.gz$', name)
+    if m:
+        f1 = gzip.open(name, mode)
+    else:
+        f1 = open(name, mode)
+    return f1
+
+def closefile(f):
+    f.close()
 
 def parse(ifile, outdir, sequencenum):
     print 'infile:', ifile
@@ -36,215 +242,16 @@ def parse(ifile, outdir, sequencenum):
     print 'sequencenum:', sequencenum
     print "PID:", os.getpid()
 
-    def gettable(stpfile, start, stop, never):
-        # Find the top of the table
-        # returns the lines from the STP file between start and stop
-        # returns "None" on EOF
-        print 'gettable called: (', stpfile, ",", "start", ",", "stop", ",", never, ')'
-        debug = 0
-        if never is None:
-            never = 'xyzzy123____5551212'
-        startmatch = re.compile(start)
-        stopmatch = re.compile(stop)
-        diematch = re.compile(never)
-
-        (patternfound, rc) = (skipto(stpfile, startmatch, diematch))
-        if rc:
-            returnbuffer = io.StringIO()
-            rc = False
-            if debug > 14:
-                print 'patternfound:', patternfound
-            if patternfound is not None:
-                # Process to the bottom of the table ...
-                # for stpbuffer in stpfile:
-                while True:
-                    stpbuffer = stpfile.readline()
-                    if stpbuffer == '':
-                        break
-
-                    if stopmatch.match(stpbuffer):
-                        # print 'found the terminating regExp'
-                        rc = True
-                        break
-                    elif diematch.match(stpbuffer):
-                        returnbuffer = stpbuffer
-                        rc = False
-                        break
-                    elif stpbuffer is None:
-                        returnbuffer = None
-                    else:
-                        returnbuffer.write(unicode(stpbuffer))
-            else:
-                returnbuffer = None
-                patternfound = ''
-                rc = False
-        else:
-            patternfound = None
-            returnbuffer = None
-            rc = False
-        return patternfound, returnbuffer, rc
-
-    def skipto(stpfile, firstregx, dieregx):
-        print "skipto(", ",", stpfile, ",", 'firstRE', ",", dieregx, ")"
-        # Look forward into the file
-        # searching for RE or dieRE
-        # return rc = True if matching RE
-        # return rc = False if matching dieRE
-        found = False
-        debug = 20
-        i = 0
-        while not found:
-            fileptr = stpfile.tell()  # Where are we before the read?
-            stpline = stpfile.readline()
-            if stpline == '':
-                return '', False  # EOF?
-            i += 1
-            if firstregx.match(stpline):
-                if debug > 19:
-                    print 'matched firstRE', stpline  # print 'skipped',i,'lines'
-                return stpline, True
-            elif dieregx.match(stpline):
-                if debug > 19:
-                    print 'matched dieRE', stpline
-                stpfile.seek(fileptr)  # roll the file back
-                print 'skipped', i, 'lines'
-                return stpline, False
-
-    def tsdecode(ts):
-        # Input - a time stamp from STP formated like: <TIMESTAMP: 20141107, 080003>
-        # Returns - ctime long integer
-
-        sts = ts.split()
-        year = int((sts[1])[0:4])
-        month = int((sts[1])[4:6])
-        day = int((sts[1][6:8]))
-        hour = int((sts[2][0:2])) - 1
-        minute = int((sts[2][2:4]))
-        second = int((sts[2][4:6]))
-        t = (year, month, day, hour, minute, second, 0, 0, -1)
-        # print ts, year, month, day, hour, minute, second
-        retval = int(time.mktime(t))
-        return retval
-
-    def setval(trackingvar, table, i, rateflag):
-        # Tag column i of table as a rate field
-        # This is probably very un-pythonesque.
-        # if the table hasn't been seen before, create a row
-        try:
-            trackingvar[table][i] = rateflag
-            rc = True
-        except KeyError:
-            row = [None] * maxrow
-            trackingvar[table] = row
-            trackingvar[table][i] = rateflag
-            rc = True
-        return rc
-
-    def getval(trackingvar, table, i):
-        # Given a table name and column
-        # Returns True if this is
-        # rate column
-        if not trackingvar[table][i]:
-            rc = None
-        else:
-            rc = trackingvar[table][i]
-        return rc
-
-    def printdb(trackingvar):
-        # Convenience routine for debugging
-        for key in trackingvar:
-            print key,
-            i = 0
-            while trackingvar[key][i] is not None:
-                print trackingvar[key][i],
-                i += 1
-            print
-
-    def processheaders(fp):
-        # Is there a clean way to do this without reading the whole file ?
-        # Does it matter if the net effect is that the file will be in buffer cache for the 2nd pass ?
-        print "Processing Headers and Metrics"
-        debug = 10
-        while True:
-            (firstline, tabletext, rc) = gettable(fp, '^<METRIC:', '^<END>', '^<TIMESTAMP: ')
-            # if tabletext is None or rc == False:
-            if tabletext is None:
-                print 'out of metrics?'
-                break  # We either ran out of Metrics in the file or didn't see an END.
-            else:
-                tablename = firstline[9:-3]
-                # For unknown reasons, 4 variables are named RDF-* in the metric section and Rdf-* in the Data section
-                # Patch around this for now.
-                if tablename[0:4] == 'RDF-':
-                    tablename = re.sub("RDF-", "Rdf-", tablename)
-                if debug > 9:
-                    print "Metric Name: " + tablename
-                mybuffer = tabletext.getvalue().split("\n")
-                columns = []
-                colnum = 0
-                for line in mybuffer:
-                    # For each column there is a CSV with a name and some attributes.
-                    # Similar to:
-                    # symid,long,Key,ArchiveLast
-                    # ios per sec,longlong,ConvertToRate,ArchiveStats,sortDescending
-                    # reads per sec,longlong,ConvertToRate,ArchiveStats
-
-                    if line != '':
-                        linevalues = line.split(',')
-                        columnname = linevalues[0]
-                        columntype = linevalues[1]
-                        setval(typetable, tablename, colnum, columntype)
-                        columnaction = linevalues[2]
-                        if columnaction == "Derived":
-                            setval(ratetable, tablename, colnum, False)
-                        elif columnaction == "ConvertToRate":
-                            setval(ratetable, tablename, colnum, True)
-                        else:
-                            setval(ratetable, tablename, colnum, False)
-                        setval(headertable, tablename, colnum, columnname)
-                        columns.append(columnname)
-                    colnum += 1
-                headers[tablename] = columns
-        # Build header string in global variable, one for each table.
-
-        return headers, True
-
-    def openoutputfile(filename, directoryname, headers):
-        # Convenience routine
-        # Check to see if the file exists.  If not, create it and place headers
-        # else open for append
-        fullyqualifiedname = directoryname + '/' + filename
-
-        if not os.path.isfile(fullyqualifiedname):
-            # Here when the file doesn't already exist
-            f1 = open(fullyqualifiedname, 'a')
-            f1.write(headers)
-        else:
-            f1 = open(fullyqualifiedname, 'a')
-        return f1
-
-    def openinputfile(name, mode):
-        # print 'file is:', name
-        # print 'directory is:',os.getcwd()
-        m = re.search('.*.gz$', name)
-        if m:
-            f1 = gzip.open(name, mode)
-        else:
-            f1 = open(name, mode)
-        return f1
-
-    def closefile(f):
-        f.close()
-
     # Global table which tracks which variables are rates and need additional processing
     # True if the column is a Rate
     # Key is tableName_Column
-    lastt = 0
-    deltat = 0
-    ratetable = {}
-    typetable = {}
-    headertable = {}
-    headers = {}
+    global lastt
+    global deltat
+    global ratetable
+    global typetable
+    global headertable
+    global headers
+    global maxrow
 
     directory = os.getcwd() + '/' + 'data' + '/' + str(sequencenum)  # Output directory, mkdir if needed.
     if not os.path.exists(directory):
@@ -338,8 +345,8 @@ def parse(ifile, outdir, sequencenum):
                                 newvalue = val
                                 oldvalue = priorrow[key][i]
                                 if (oldvalue != '') and (oldvalue != " "):
-                                    if getVal(rateTable, table, i):
-                                        type = getVal(typeTable,table,i)
+                                    if getval(ratetable, table, i):
+                                        type = getval(typetable,table,i)
                                         if type != 'float':
                                             nv = long(newvalue)
                                             ov = long(oldvalue)
@@ -374,33 +381,72 @@ def parse(ifile, outdir, sequencenum):
                     else:
                         firstsample[table] = False
                         lastt = t
-                CloseFile(outfile)
-    CloseFile(f)
+                closefile(outfile)
+    closefile(f)
     return os.getpid()
 
+def fCompletecb(result):
+    # This is a callback, which is invoked when a spawned process completes.
+    global numprocs
+    numprocs -= 1
+    logging.info ("fCompletecb called")
+    print result
+
+def Timeout():
+    logging.info('timer expired, shutting down.')
+    global numprocs
+    numprocs = 0
+    time.sleep(10)
+    sys.exit()
 #
 # MAIN
 #
 if __name__ == "__main__":
-    debug = 0
-    maxrow = 150
+
+    # Globals
+    debug = 0   # Debug level
+    maxrow = 150   # Largest number of variables on a row of input data. (i.e. column width)
+    numprocs = 0
+    lastt = 0
+    deltat = 0
+    ratetable = {}
+    typetable = {}
+    headertable = {}
+    headers = {}
+
+    logging.basicConfig(level=logging.DEBUG)
+
     # Build a sorted list of filenames
+
     inputDirectory = './'
     inputFiles = inputDirectory + 'T1*'
     ifile = iglob(inputFiles)
+
     results = []
-    pool = mp.Pool(processes=8)
-    seq = os.getpid()
+    nprocs = mp.cpu_count()
+    mp.log_to_stderr(logging.DEBUG)
+    logging.info("starting {} processes in a pool".format(nprocs))
+    pool = mp.Pool(processes=nprocs)
+    seqnum = os.getpid()
+    i = 0
     for infile in sorted(ifile):
-        r = pool.apply_async(parse, args=(infile, '.', seq))
-        results.append(r)
-        seq += 1
+        i += 1
+        logging.info('{}: submitting file {}'.format(i,infile) )
+        r = pool.apply_async(parse, args=(infile, '.', i), callback = fCompletecb)
+        numprocs += 1
+    sys.exit()
 
-    for r in results:
-        try:
-            print 'results check:'
-            print(r.get(timeout=3))
-        except Exception: pass
-
+    # Wait for maxtime or for all processes to complete.
+    # which ever comes first.
+    TIMER = False
+    if (TIMER):
+        maxtime = 10000000
+        t = Timer(maxtime,Timeout)
+        t.start()
+    # Spin, while the queue processes
+    while (numprocs > 0):
+        print 'numprocs =', numprocs
+        time.sleep(3)
 
     print "complete"
+
